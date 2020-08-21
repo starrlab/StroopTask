628d5bed396bbf97b56e15ed7f03d846676d39b8
unknown <mrrandyperrone@gmail.com>
2020-08-21 15:30:00 -0700

    Added code to have quick pause when incorrect response

--- practiceCode.js
+++ practiceCode.js
@@ -13,6 +13,9 @@ const FIXATION_DURATION = [2000, 2250, 2500, 2750, 3000]; //Sets the fixation du
 const FIXATION_KEY = '+';
 const FIXATION_SIZE = 60;
 
+const ERROR_DISPLAY_LENGTH = 5000;
+const ERROR_DISPLAY_MESSAGE = "You messed up A-A-Ron!";
+
 const KEYBOARD_PRESS_HAPPY = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(39); //This is the arrow key code
 const KEYBOARD_PRESS_FEAR = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(37); //This is the arrow key code
 let timeline = [];
@@ -34,14 +37,14 @@ timeline.push(instructions);
 /***********Image variables (pre-loaded)*************/
 let f_c_m_hi_4 = {stimulus: "img/f_c_m_hi_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
 let f_i_f_as_4 = {stimulus: "img/f_i_f_as_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
-let h_c_f_hi_4 = {stimulus: "img/h_c_f_hi_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
+let h_c_f_hi_4 = {stimulus: "img/h_c_f_hi_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_HAPPY }}
 let f_c_m_af_4 = {stimulus: "img/f_c_m_af_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
-let h_i_m_ca_4 = {stimulus: "img/h_i_m_ca_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
+let h_i_m_ca_4 = {stimulus: "img/h_i_m_ca_4.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_HAPPY }}
 let f_i_f_hi_1 = {stimulus: "img/f_i_f_hi_1.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
 let f_c_f_af_1 = {stimulus: "img/f_c_f_af_1.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
-let h_i_m_ca_3 = {stimulus: "img/h_i_m_ca_3.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
+let h_i_m_ca_3 = {stimulus: "img/h_i_m_ca_3.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_HAPPY }}
 let f_i_m_hi_3 = {stimulus: "img/f_i_m_hi_3.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
-let h_c_m_as_1 = {stimulus: "img/h_c_m_as_1.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_FEAR }}
+let h_c_m_as_1 = {stimulus: "img/h_c_m_as_1.png",data: { test_part: 'test', correct_response: KEYBOARD_PRESS_HAPPY }}
 
 sequence = [f_c_m_hi_4, f_i_f_as_4, h_c_f_hi_4, f_c_m_af_4, h_i_m_ca_4, f_i_f_hi_1, f_c_f_af_1, h_i_m_ca_3, f_i_m_hi_3, h_c_m_as_1];
 
@@ -75,6 +78,10 @@ let test = {
     on_finish: function (data) {
         data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
         data.user_response = jsPsych.pluginAPI.convertKeyCodeToKeyCharacter(data.key_press);
+        if(jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response) != data.key_press){
+            console.log('wrong key');
+            skipNextTrialFunc();
+        }
     }
 };
 
@@ -87,9 +94,42 @@ let test_procedure = {
 //add this to the timeline
 timeline.push(test_procedure);
 
+/***********Debrief Screen************/
+    //allows you to show user statistics of trials
+let debrief_block = {
+        type: "html-keyboard-response",
+        stimulus: function () {
+
+            var trials = jsPsych.data.get().filter({ test_part: 'test' });
+            var correct_trials = trials.filter({ correct: true });
+            var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+            var rt = Math.round(correct_trials.select('rt').mean());
+
+            return "<p>You got <strong>" + accuracy + "%</strong> correct.</p>" +
+                "<p>Average response time was <strong>" + rt + "ms</strong>.</p>" +
+                "<p>Press any key to complete the experiment. Thank you!</p>";
+
+        },
+        on_load: function () {
+            //let filename = "task_" + Date.now().toString() + "_ver" + VERSION + ".json";
+            //saveData(jsPsych.data.get().json(), filename);
+        }
+    };
+//add this to timeline
+timeline.push(debrief_block);
+
 
 /*********Start Experiment************/
 //Display data shows the data displayed at end of trials
 jsPsych.init({
     timeline: timeline
 });
+
+const skipNextTrialFunc = () => {
+    //document.getElementById("error-header").innerHTML = ERROR_DISPLAY_MESSAGE;
+    jsPsych.pauseExperiment();
+    setTimeout(function(){
+        //document.getElementById("error-header").innerHTML = '';
+        jsPsych.resumeExperiment();
+    }, ERROR_DISPLAY_LENGTH);
+}
