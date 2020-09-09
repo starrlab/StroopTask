9a26ae362f14d3f05fec3fda7c3cf0d6a429bcb5
unknown <mrrandyperrone@gmail.com>
2020-09-09 12:56:09 -0700

    Changed verbage for tutorial for enter key

--- taskCode.js
+++ taskCode.js
@@ -361,14 +361,14 @@ let EStutorial1 = {
         " and you will also see red text written over these faces.</p>" +
         "<p>Please ignore the red text and indicate the facial expression type: happy or fear by entering on your keyboard " +
         "<strong>" + KEYBOARD_PRESS_HAPPY + "</strong> for happy and <strong>" + KEYBOARD_PRESS_FEAR + "</strong> for fear.</p>" +
-        "<p><strong>-Press any key to continue-</strong></p>"
+        "<p><strong>-Press ENTER key to continue-</strong></p>"
 };
 
 let EStutorial2 = {
     type: "html-keyboard-response",
     choices: [KEYBOARD_PRESS_TUTORIAL],
     stimulus: "<p>Here is an example of one of the stimuli you will see in the task.  For this image, the <strong>facial expression</strong> is <strong>happy</strong>; so you would enter → for happy</p>" +
-        "<p>Press any key to continue</p><br>" +
+        "<p>Press ENTER key to continue</p><br>" +
         "<div><img src='img/ES/h_c_m_hi_1.png'/>" +
         "<p><strong>Facial Expression - Happy</strong><br>Word Meaning - Happy</p>" +
         "</div>",
@@ -378,7 +378,7 @@ let EStutorial3 = {
     type: "html-keyboard-response",
     choices: [KEYBOARD_PRESS_TUTORIAL],
     stimulus: "<p>Here is an example of one of the stimuli you will see in the task.  For this image, the <strong>facial expression</strong> is <strong>fear</strong>; so you would enter ← for fear</p>" +
-        "<p>Press any key to continue</p><br>" +
+        "<p>Press ENTER key to continue</p><br>" +
         "<div><img src='img/ES/f_c_f_ca_1.png'/>" +
         "<p><strong>Facial Expression - Fear</strong><br>Word Meaning - Happy</p>" +
         "</div>",
